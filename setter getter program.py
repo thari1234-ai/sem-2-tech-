feba77e989465fccaf81999e92e59ea8ff5a44a2
thari1234-ai <thariniparthasarathy1804@gmail.com>
2024-12-16 11:46:08 +0530

    Update setter getter program.py

--- setter getter program.py
+++ setter getter program.py
@@ -17,3 +17,24 @@ s=Student(name,age)
 
 print(s.get_name())
 print(s.get_age())
+
+###same but by passing smtg
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+    def set_name(self,name):
+        self.name=name
+    def set_age(self,age):
+        self.age=age
+        
+name=input()
+age=int(input())
+s=Student(name,age)
+s.set_name(input())
+print(s.get_name())
+print(s.get_age())
